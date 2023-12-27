7ef17287872563a68bb5e8eb2dac0cd36068995e
roninds14 <roninds14@gmail.com>
2023-12-27 19:43:57 -0300

    Add endpoint category

--- backend/src/main/java/com/roninds/dscatalog/entities/Category.java
+++ backend/src/main/java/com/roninds/dscatalog/entities/Category.java
@@ -1,2 +1,17 @@
-package com.roninds.dscatalog.entities;public class Category {
+package com.roninds.dscatalog.entities;
+
+import lombok.*;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class Category implements Serializable {
+
+    private  Long id;
+    private String name;
 }
--- backend/src/main/java/com/roninds/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/roninds/dscatalog/resources/CategoryResource.java
@@ -1,2 +1,25 @@
-package com.roninds.dscatalog.resources;public class CategoryResource {
+package com.roninds.dscatalog.resources;
+
+import com.roninds.dscatalog.entities.Category;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @GetMapping
+    public ResponseEntity<List<Category>> findAll() {
+
+        List<Category> list = new ArrayList<>();
+        list.add(new Category(1L, "Books"));
+        list.add(new Category(2L, "Electronics"));
+
+        return ResponseEntity.ok().body(list);
+    }
 }
